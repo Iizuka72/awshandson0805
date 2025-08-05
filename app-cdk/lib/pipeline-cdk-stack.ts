6bb7dd3a404c078cf183c9d0c54b100ae7b0b342
your-user-name <your-email-address>
2025-08-05 02:38:49 +0000

    Implemented CI pipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,6 +1,9 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_codeconnections as codeconnections } from 'aws-cdk-lib';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -11,6 +14,51 @@ export class PipelineCdkStack extends Stack {
         providerType: 'GitHub',
       });
       
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+      pipelineType: codepipeline.PipelineType.V2,
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+    });
+
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+        stageName: 'Source',
+        actions: [
+          new codepipeline_actions.CodeStarConnectionsSourceAction({
+            actionName: 'GitHub',
+            owner: 'Iizuka72',
+            repo: 'awshandson0805',
+            output: sourceOutput,
+            branch: 'main',
+            connectionArn: 'arn:aws:codeconnections:ap-northeast-1:740299724154:connection/81bf2624-6d47-4bd2-914f-741c490de4d4',
+          }),
+        ],
+      });
+
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Unit-Test',
+          project: codeBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
     new CfnOutput(this, 'SourceConnectionArn', {
         value: SourceConnection.attrConnectionArn,
       });
