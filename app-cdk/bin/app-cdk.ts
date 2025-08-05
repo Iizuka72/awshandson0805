6e7ff0e6f5f74bbf3172fa289881a94bf65c8402
your-user-name <your-email-address>
2025-08-05 01:41:55 +0000

    Initial Commit

---
+++ app-cdk/bin/app-cdk.ts
@@ -0,0 +1,8 @@
+
+import * as cdk from 'aws-cdk-lib';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+
+const app = new cdk.App();
+const testCdkStack = new AppCdkStack(app, 'test', {});
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
